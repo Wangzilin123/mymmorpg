357044568ff57fc3c1f6616b92540fb9e6ab6e28
DESKTOP-I3NG2D0\王梓霖 <mvp2743003389@163.com>
2025-06-26 22:48:46 +0800

    4.4 部分UI

---
+++ Src/MyClient/Assets/Scripts/UI/UICharacterSelect.cs
@@ -0,0 +1,54 @@
+﻿using Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterSelect : MonoBehaviour {
+
+    [SerializeField] private GameObject panelCreate;
+    [SerializeField] private GameObject panelSelect;
+
+    [SerializeField] private GameObject btnCreateCancel;
+
+    [SerializeField] private InputField charName;
+    //CharacterClass charClass;
+
+    [SerializeField] private Transform uiCharList;
+    [SerializeField] private GameObject uiCharInfo;
+
+    [SerializeField] private List<GameObject> uiChars = new List<GameObject>();
+
+    private int selectCharacterIdx = -1;
+
+    public UICharacterView characterView;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitCharacterSelect(true);
+
+    }
+	
+    public void InitCharacterSelect(bool init)
+    {
+        panelCreate.SetActive(false);
+        panelSelect.SetActive(true);
+
+        if (init)
+        {
+            foreach (var old in uiChars)
+            {
+                Destroy(old);
+            }
+            uiChars.Clear();
+
+            for (int i = 0; i < User.Instance.Info.Player.Characters.Count; i++)
+            {
+                GameObject go = Instantiate(uiCharInfo,this.uiCharList);
+                UICharInfo chrInfo = go.GetComponent<UICharInfo>();
+                //chrInfo.
+            }
+        }
+    }
+}
