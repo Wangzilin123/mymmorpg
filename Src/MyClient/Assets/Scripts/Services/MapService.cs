13463abdf627c6a880b1dd9b7ca1a251a8a23b1c
DESKTOP-I3NG2D0\王梓霖 <mvp2743003389@163.com>
2025-08-25 01:16:19 +0800

    4.6（2） 38：00

---
+++ Src/MyClient/Assets/Scripts/Services/MapService.cs
@@ -0,0 +1,69 @@
+﻿using Common.Data;
+using Models;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Services
+{
+    class MapService : Singleton<UserService>, IDisposable
+    {
+        public MapService()
+        {
+            MessageDistributer.Instance.Subscribe<SkillBridge.Message.MapCharacterEnterResponse>(this.OnMapCharacterEnter);
+            MessageDistributer.Instance.Subscribe<SkillBridge.Message.MapCharacterLeaveResponse>(this.OnMapCharacterLeave);
+        }
+
+        public int CurrentMapId { get; private set; }
+
+        public void Dispose()
+        {
+            MessageDistributer.Instance.Unsubscribe<SkillBridge.Message.MapCharacterEnterResponse>(this.OnMapCharacterEnter);
+            MessageDistributer.Instance.Unsubscribe<SkillBridge.Message.MapCharacterLeaveResponse>(this.OnMapCharacterLeave);
+        }
+        
+        public void Init()
+        {
+
+        }
+
+        private void OnMapCharacterEnter(object sender, MapCharacterEnterResponse response)
+        {
+            Debug.LogFormat("OnMapCharacterEnter:Map:{0} Count:{1}", response.mapId, response.Characters.Count);
+            foreach (var cha in response.Characters)
+            {
+                if (User.Instance.CurrentCharacter.Id == cha.Id)
+                {//当前角色切换地图
+                    User.Instance.CurrentCharacter = cha;
+                }
+                CharacterManager.Instance.AddCharacter(cha);
+            }
+            if (CurrentMapId != response.mapId)
+            {
+                this.EnterMap(response.mapId);
+                this.CurrentMapId = response.mapId;
+            }
+        }
+        private void OnMapCharacterLeave(object sender, MapCharacterLeaveResponse response)
+        {
+
+        }
+
+        private void EnterMap(int mapId)
+        {
+            if (DataManager.Instance.Maps.ContainsKey(mapId))
+            {
+                MapDefine map = DataManager.Instance.Maps[mapId];
+                SceneManager.Instance.LoadScene(map.Resource);
+            }
+            else
+                Debug.LogErrorFormat("EnterMap: Map {0} not existed", mapId);
+        }
+
+    }
+}
